711f0ce78074847a4926cfd4a065eca3b1d3b911
Script <2271277962@qq.com>
2025-11-03 20:58:46 +0800

    上传设备树

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TB372FC.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TB372FC-user \
+    omni_TB372FC-userdebug \
+    omni_TB372FC-eng
